dcba833a616d892e4e1a9d00fa2efd157b9d8680
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-14 16:05:08 +0530

    Projects Design updated

--- src/components/home/projects.vue
+++ src/components/home/projects.vue
@@ -3,19 +3,38 @@
         <hr><br>  
         <h3>Projects</h3>
         <br><hr>
-        <div class="row">
-            <div class="col-md-4" v-for="(item, i) in projectsData.projects" :key="i"> 
-                <h3 class="text-muted">
-                    {{ item.name }}
-                </h3>
-                <p>
-                    {{ item.year }} | <a v-bind:href= "item.project-url">Show Project</a>
-                </p>
-                <p>
-                    {{ item.description }}
-                </p>
+        <div class="row" v-if="!showProjects">
+            <div class="col-md-4" v-for="(item, i) in homePageProjects" :key="i"> 
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title text-muted">
+                            {{ item.name }}
+                        </h3>
+                        <p class="card-text">
+                            {{ item.description }}
+                        </p>
+                        <a :href= "item.project-url">Show Project</a>
+                    </div>
+                </div>
             </div>
         </div>
+        <button class="btn btn-primary" v-if="showButton" item-align="right" v-on:click="ShowProjects">See more</button>
+        <div class="row" v-if="showProjects">
+            <div class="col-md-4" v-for="(item, i) in projectsData.projects" :key="i"> 
+                <div class="card m-2">
+                    <div class="card-body">
+                        <h3 class="card-title text-muted">
+                            {{ item.name }}
+                        </h3>
+                        <p class="card-text">
+                            {{ item.description }}
+                        </p>
+                        <a :href= "item.project-url">Show Project</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button class="btn btn-primary" v-if="!showButton" item-align="right" v-on:click="ShowProjects">See less</button>
     </div>
 </template>
 
@@ -25,7 +44,16 @@ import projectsData from '@/assets/data/projects.json'
 export default {
     data() {
         return {
-            projectsData
+            projectsData,
+            showProjects:false,
+            showButton: true,
+            homePageProjects: projectsData.projects.slice(0,3)
+        }
+    },
+    methods: {
+        ShowProjects() {
+            this.showProjects = !this.showProjects,
+            this.showButton = !this.showButton
         }
     }
 }
