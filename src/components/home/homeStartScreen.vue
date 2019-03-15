c0db1fad63447144c873eaecaa8fecfe8e2784e1
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-15 10:37:20 +0530

    About page updated

--- src/components/home/homeStartScreen.vue
+++ src/components/home/homeStartScreen.vue
@@ -4,13 +4,7 @@
             <div class="col-md-7">
                 <h1 class="h1-responsive text-muted font-weight-bold">{{ portfolioData.name }}</h1>
                 <h6 class="text-muted">{{ portfolioData.designation }}</h6>
-                <ul class="list-group list-group-horizontal" type="none">
-                    <li v-for="(item, i) in portfolioData.socialMediaProfiles" :key="i">
-                        <a :href="item.url" target="_blank" class="fb-ic mr-3" style="color:black; " role="button">
-                            <i :class="item.icon"></i>
-                        </a>
-                    </li>
-                </ul>
+                <SocialLinks :portfolioData='{portfolioData}'></SocialLinks>
                 <router-link class="btn btn-outline-primary" to="about"> About Me </router-link>
             </div>
             <div class="col-md-5">
@@ -22,7 +16,12 @@
 
 <script>
 import portfolioData from '@/assets/data/portfolioData.json'
+import SocialLinks from '@/components/common/socialLinks.vue'
+
 export default {
+    components: {
+        SocialLinks
+    },
     methods: {
         showData() {
             this.$emit('ShowGitHubData')
