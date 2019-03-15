6770d5b87571721bf6210de9ca67bd445b6f3e7e
Varsha Jaiswal <shaluvarshajaiswal@gmail.com>
2019-03-15 10:09:48 +0530

    Home page updated

--- src/components/home/homeStartScreen.vue
+++ src/components/home/homeStartScreen.vue
@@ -2,13 +2,15 @@
     <div class="container">
         <div class="row my-5 align-items-center">
             <div class="col-md-7">
-                <h2 class="h1-responsive">{{ portfolioData.name }}</h2>
-                <p class="text-muted">{{ portfolioData.designation }}</p>
-                <p class="text-muted">
-                    <i class="fab fa-github"></i> &nbsp;
-                    <i class="fab fa-linkedin-in"></i> &nbsp;
-                    <i class="fab fa-facebook-f"></i> &nbsp;
-                </p>
+                <h1 class="h1-responsive text-muted font-weight-bold">{{ portfolioData.name }}</h1>
+                <h6 class="text-muted">{{ portfolioData.designation }}</h6>
+                <ul class="list-group list-group-horizontal" type="none">
+                    <li v-for="(item, i) in portfolioData.socialMediaProfiles" :key="i">
+                        <a :href="item.url" target="_blank" class="fb-ic mr-3" style="color:black; " role="button">
+                            <i :class="item.icon"></i>
+                        </a>
+                    </li>
+                </ul>
                 <router-link class="btn btn-outline-primary" to="about"> About Me </router-link>
             </div>
             <div class="col-md-5">
