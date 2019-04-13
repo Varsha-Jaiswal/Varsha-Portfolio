bfbcd9cbae2e0af0ad1609fa5aa831d226d044ed
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2019-04-13 13:48:25 +0530

    Minor Update

--- src/components/common/appFooter.vue
+++ src/components/common/appFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
-        <div class="footer-copyright bg-light text-center py-3">© 2019 Copyright:
-            <a href="https://mdbootstrap.com/education/bootstrap/"> Varsha Jaiswal</a>
+        <div class="footer-copyright bg-light text-center py-3">©2019 Copyright:
+            <a href="https://varsha-jaiswal.firebaseapp.com"> Varsha Jaiswal</a>
         </div>
   </footer>
 </template>
