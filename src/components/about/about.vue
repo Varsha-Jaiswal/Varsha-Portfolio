fcfe1f74ab486be2796f4c8356ce00dead0fc73c
varsha-jaiswal <shaluvarshajaiswal@gmail.com>
2019-04-13 10:25:55 +0530

    Updated About

--- src/components/about/about.vue
+++ src/components/about/about.vue
@@ -3,15 +3,15 @@
         <div class="row my-5 align-items-center">
             <div class="col-md-7">
                 <h2 class="h1-responsive">
-                    I'm <mark style="color:red">{{ portfolioData.name }}</mark>
+                    I'm <strong>{{ portfolioData.name }}</strong>
                 </h2>
                 <br><hr><br>
-                <p class="text-muted">{{ portfolioData.about }}</p>
-                <SocialLinks :portfolioData='{portfolioData}'></SocialLinks>
+                <p class="text-muted">{{ portfolioData.about }}</p><br>
+                <SocialLinks :portfolioData='{portfolioData}'></SocialLinks><br>
                 <a href="../../assets/resume/Varsha_Resume.pdf" class="btn btn-outline-primary" download="Varsha_Resume.pdf">Download Resume</a>
             </div>
             <div class="col-md-5">
-                <img src="https://gdgjalandhar.com/img/varsha.380e53ce.jpg" class="rounded-circle" width="50%" alt="" srcset="">
+                <img src="../../assets/image/varsha.jpg" class="rounded-circle" width="50%" alt="" srcset="">
             </div>
         </div>
     </div>
